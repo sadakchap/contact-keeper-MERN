551f67eafc0da71637cd94fb3ed1d49bebcbdf7f
sadakchap <aliceprerna@gmail.com>
2020-09-07 14:06:25 +0530

    added Routes files

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const contactRoutes = require("./routes/contact");
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.status(200).json({
+        msg: 'Welcome to ContactKeeper API..😍😍'
+    });
 });
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/contact', contactRoutes);
+
 
 const PORT = process.env.PORT || 1930;
 app.listen(PORT, () => console.log(`Server is up & running at ${PORT}`))
