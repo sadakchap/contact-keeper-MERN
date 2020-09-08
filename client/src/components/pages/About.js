54ff7ed50269420062851ffcb30bcce9b79006f9
sadakchap <aliceprerna@gmail.com>
2020-09-08 19:05:19 +0530

    created Navbar, Home, About components & implemented react-router-dom

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores similique tempore natus 
+                non vel deserunt eos cumque illum? Voluptas sunt delectus iste, voluptatem atque cupiditate 
+                natus qui quo quis accusantium? Lorem ipsum dolor sit amet consectetur adipisicing elit.  Dolores
+                similique tempore natus non vel deserunt eos cumque illum? Voluptas sunt delectus iste, 
+                voluptatem atque cupiditate natus qui quo quis accusantium?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores similique tempore natus 
+                non vel deserunt eos cumque illum? Voluptas sunt delectus iste, voluptatem atque cupiditate 
+                natus qui quo quis accusantium? Lorem ipsum dolor sit amet consectetur adipisicing elit.  Dolores
+                similique tempore natus non vel deserunt eos cumque illum? Voluptas sunt delectus iste, 
+                voluptatem atque cupiditate natus qui quo quis accusantium?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores similique tempore natus 
+                non vel deserunt eos cumque illum? Voluptas sunt delectus iste, voluptatem atque cupiditate 
+                natus qui quo quis accusantium? Lorem ipsum dolor sit amet consectetur adipisicing elit.  Dolores
+                similique tempore natus non vel deserunt eos cumque illum? Voluptas sunt delectus iste, 
+                voluptatem atque cupiditate natus qui quo quis accusantium?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores similique tempore natus non vel 
+                deserunt eos cumque illum? Voluptas sunt delectus iste, voluptatem atque cupiditate natus qui quo 
+                quis accusantium?
+            </p>
+        </div>
+    )
+}
+
+export default About
